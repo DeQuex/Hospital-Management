31b27eb6f5578246979c982fb49c9663d5145b71
Ataberk Öztürk <ataberkus@hotmail.com.tr>
2021-02-18 15:48:15 +0300

    test

--- Hospital Management/Form1.cs
+++ Hospital Management/Form1.cs
@@ -16,5 +16,10 @@ namespace Hospital_Management
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
