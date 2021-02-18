ebd26128013ff00e79f13cd680a0d4f0819ae6b3
Ataberk Öztürk <ataberkus@hotmail.com.tr>
2021-02-18 15:59:23 +0300

    added account classes and rename hospital management name to gui

---
+++ Account Management/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Management
+{
+    public class Account
+    {
+        private string account_type;
+        private string account_username;
+        private string account_password;
+
+        public Account(string uAccountType, string uAccountUsername, string uAccountPassword)
+        {
+            account_type = uAccountType;
+            account_username = uAccountUsername;
+            account_password = uAccountPassword;
+        }
+
+        public string GetAccountType()
+        {
+            return account_type;
+        }
+
+        public string GetAccountUsername()
+        {
+            return account_username;
+        }
+
+        public string GetAccountPassword()
+        {
+            return account_password;
+        }
+
+        public void SetAccountType(string type)
+        {
+            account_type = type;
+        }
+
+        public void SetAccountUsername(string username)
+        {
+            account_username = username;
+        }
+
+        public void SetAccountPassword(string password)
+        {
+            account_password = password;
+        }
+    }
+}
---
+++ Account Management/AccountList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Management
+{
+    public class AccountList
+    {
+        private List<Account> accounts;
+
+        public AccountList()
+        {
+            accounts = new List<Account>();
+        }
+
+        public void addAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        public void delAccount(Account account)
+        {
+            accounts.Remove(account);
+        }
+
+        public List<Account> GetList()
+        {
+            return accounts;
+        }
+    }
+}
