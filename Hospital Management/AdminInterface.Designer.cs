3b091b546e5e2744e90b44667533435fcac14147
Savas07 <kelepce1400@gmail.com>
2021-05-09 18:47:33 +0300

    nothing to push

--- Hospital Management/AdminInterface.Designer.cs
+++ Hospital Management/AdminInterface.Designer.cs
@@ -30,26 +30,50 @@ namespace Hospital_Management
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(890, 515);
+            this.label1.Location = new System.Drawing.Point(896, 438);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 31);
+            this.label1.Size = new System.Drawing.Size(116, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Sikiş";
+            this.label1.Text = "Deneme";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1718, 968);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Deneme";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1561, 992);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Deneme";
             // 
             // AdminInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "AdminInterface";
-            this.Size = new System.Drawing.Size(1920, 1080);
+            this.Size = new System.Drawing.Size(1680, 900);
             this.Load += new System.EventHandler(this.AdminInterface_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -59,5 +83,7 @@ namespace Hospital_Management
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Hospital Management/Functions.cs
+++ Hospital Management/Functions.cs
@@ -12,7 +12,7 @@ namespace Hospital_Management
     {
         public static void CenterUserControl(Form fName, Control name)
         {
-            name.Location = new Point(fName.Size.Width / 2 - (name.Size.Width / 2), fName.Size.Height / 2 - (name.Size.Height / 2));
+             name.Location = new Point(fName.Size.Width / 2 - (name.Size.Width / 2), fName.Size.Height / 2 - (name.Size.Height / 2));
         }
         public static void ResizeForm(Form fName, Control name)
         {
