030200514a177b678c83824d3786ca99ea06e46e
Serdar Cesur <serdar.cesur2018@pru.edu.tr>
2021-05-27 16:23:14 +0300

    Ajanda eklendi düzenlemeler yapıldı

--- Hospital Management/DoctorInterface.cs
+++ Hospital Management/DoctorInterface.cs
@@ -24,21 +24,6 @@ namespace Hospital_Management
             Functions.OrderControl(this, Functions.Direction.Horizontal, 10, sidePanel, panel22);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            information.Visible = true;
-            operation.Visible = false;
-            panel22.Visible = false;
-        }
-
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            information.Visible = false;
-            operation.Visible = true;
-            panel22.Visible = false;
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             information.Visible = false;
@@ -51,6 +36,38 @@ namespace Hospital_Management
             Functions.OrderControl(this, Functions.Direction.Horizontal, 10, sidePanel, information);
             Functions.OrderControl(this, Functions.Direction.Horizontal, 10, sidePanel, operation);
             Functions.OrderControl(this, Functions.Direction.Horizontal, 10, sidePanel, panel22);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            information.Visible = true;
+            operation.Visible = false;
+            panel22.Visible = false;
+            ajanda.Visible = false;
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            information.Visible = false;
+            operation.Visible = true;
+            panel22.Visible = false;
+            ajanda.Visible = false;
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            information.Visible = false;
+            operation.Visible = false;
+            panel22.Visible = true;
+            ajanda.Visible = false;
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            information.Visible = false;
+            operation.Visible = false;
+            panel22.Visible = false;
+            ajanda.Visible = true;
         }
     }
 }
